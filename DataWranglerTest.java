13af125d59b42a77e4a56ba18b07823f330c3c77
Surya Narne <snarne@wisc.edu>
2022-10-11 19:47:40 -0500

    add integration tests

--- DataWranglerTest.java
+++ DataWranglerTest.java
@@ -1,107 +1,82 @@
-import java.io.FileNotFoundException; 
-import java.util.List; 
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class DataWranglerTest 
 {
   public static void main(String[] args)
   {
-      test1();
-      test2();
-      test3();
-      test4("9780345453747");
-      test5("Rowling");
+	  System.out.println("DataWrangler Individual Test 1: " + (test1() ? "passed" : "failed"));
+      System.out.println("DataWrangler Individual Test 2: " + (test2() ? "passed" : "failed"));
+      System.out.println("DataWrangler Individual Test 3: " + (test3() ? "passed" : "failed"));
+      System.out.println("DataWrangler Individual Test 4: " + (test4() ? "passed" : "failed"));
+      System.out.println("DataWrangler Individual Test 5: " + (test5() ? "passed" : "failed"));
+      System.out.println("DataWrangler Integration Test 1: " + (test6() ? "passed" : "failed"));
+      System.out.println("DataWrangler Integration Test 2: " + (test7() ? "passed" : "failed"));
       
   }
   
   //this method is to check if the path is null 
   public static boolean test1()
   {
-    
-	  System.out.println("Test1: ");
-	
-	  BookLoader obj = new BookLoader();
-	  
-	  List<IBook> books1;
-	  
-	  String path = null;
-	  
-	  
-	 try 
-	 {
-		books1 = obj.loadBooks(path);
-	} 
-	 
-	 catch (Exception e) 
-	 {
-		// TODO Auto-generated catch block
-		 System.out.println("true");
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 return true;
-	}
-	 
-	 
-	 System.out.println("false");
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 
-	 
-	 return false;
+
+          BookLoader obj = new BookLoader();
+          
+          List<IBook> books1;
+          
+          String path = null;
+          
+          
+         try 
+         {
+                books1 = obj.loadBooks(path);
+        } 
+         
+         catch (Exception e) 
+         {
+                // TODO Auto-generated catch block
+               
+                 return true;
+        }
+         
+         return false;
 
   }
   
   //prints all the books present in the csv file
   public static boolean test2()
   {
-		 
-		 
-	  BookLoader obj = new BookLoader();
-	  
-	  List<IBook> books;
-	  
-	  System.out.println("Test2: ");
-	  
-	 try 
-	 {
-		books = obj.loadBooks("books.csv");
-		for(IBook b:books)
-		{
-			System.out.println("true");
-			
-			return true;
-			
-			
-		}
-		
-		
-		
-	} 
-	 
-	 catch (FileNotFoundException e) 
-	 {
-		// TODO Auto-generated catch block
-		 System.out.println("false");
-		 
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 return false;
-		  
-	}
-	 
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 
-	 
-	 return true;
+                 
+                 
+          BookLoader obj = new BookLoader();
+          
+          List<IBook> books;
+          
+         try 
+         {
+                books = obj.loadBooks("books.csv");
+                for(IBook b:books)
+                {
+             
+
+                        return true;
+
+
+                }
+
+
+
+        } 
+         
+         catch (FileNotFoundException e) 
+         {
+                // TODO Auto-generated catch block
+
+                 return false;
+                  
+        }
+
+         
+         return true;
 
 
   }
@@ -112,164 +87,206 @@ public class DataWranglerTest
 //this method returns the count of books present in the csv file
 public static boolean test3()
 {
-	BookLoader obj = new BookLoader();
-	
-	List<IBook> books;
-	
-	System.out.println("Test3: ");
-	
-	try
-	{
-		books = obj.loadBooks("books.csv");
-		
-		int bookCount = books.size();
-		
-		if(bookCount != 0)
-		{
-			 System.out.println("true");
-		}
-	}
-	
-	catch (FileNotFoundException e) 
-	 {
-		// TODO Auto-generated catch block
-		 System.out.println("False");
-		 
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 
-		 return false;
-		  
-	}
-	
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	return true;
+        BookLoader obj = new BookLoader();
+
+        List<IBook> books;
+
+  
+
+        try
+        {
+                books = obj.loadBooks("books.csv");
+
+                int bookCount = books.size();
+
+                if(bookCount != 0)
+                {
+                     return true;
+                }
+        }
+
+        catch (FileNotFoundException e) 
+         {
+                // TODO Auto-generated catch block
+             
+                 return false;
+                  
+        }
+
+        return true;
 }
 
 //method returns details of a book through isbn
-public static boolean test4(String isbn)
+public static boolean test4()
 {
+        String isbn = "9780976540601";
 
-	BookLoader obj = new BookLoader();
-	
-	List<IBook> books;
-	
-	System.out.println("Test4: ");
-	
-	if(isbn == "")
-	{
-		System.out.println("False");
-		
-		return false;
-	}
-	
-	try
-	{
-		books = obj.loadBooks("books.csv");
-		
-		
-		
-		for(IBook b: books)
-		{
-			if(b.getISBN13().equals(isbn))
-			{
-				System.out.println("true");
-				return true;
-			}
-				
-		}
-		
-		
+        BookLoader obj = new BookLoader();
+
+        List<IBook> books;
+
+
+        try
+        {
+                books = obj.loadBooks("books.csv");
+
+
+
+                for(IBook b: books)
+                {
+                        if(b.getISBN13().equals(isbn))
+                        {
+                            
+                                return true;
+                        }
+
+                }
+
+
 }
-	
-	catch (FileNotFoundException e) 
-	 {
-		// TODO Auto-generated catch block
-		 System.out.println("False");
-		 
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 
-		 return false;
-		 
-		  
-	}
-	
-	 System.out.println("false");
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-	 System.out.println();
-  
-	 return false;
+
+        catch (FileNotFoundException e) 
+         {
+                // TODO Auto-generated catch block
+      
+                 return false;
+                 
+                  
+        }
+
+ 
+         return false;
 }
 
 //method returns details of a book through authors
-public static boolean test5(String author)
+public static boolean test5()
 {
+        String author = "Rowling";
+
     BookLoader obj = new BookLoader();
-	
-	List<IBook> books;
-	
-	System.out.println("Test5: ");
-	
-	if(author == "")
-	{
-		System.out.println("false");
-		
-		return false;
-	}
-	
-	try
-	{
-		books = obj.loadBooks("books.csv");
-		
-		for(IBook b: books)
-		{
-			if(b.getAuthors().contains(author))
-			{
-				System.out.println("true");
-				
-				return true;
-			}
-				
-		}
+
+        List<IBook> books;
+
+      
+
+        if(author == "")
+        {
+
+                return false;
+        }
+
+        try
+        {
+                books = obj.loadBooks("books.csv");
+
+                for(IBook b: books)
+                {
+                        if(b.getAuthors().contains(author))
+                        {
+
+                                return true;
+                        }
+
+                }
 }
-	
-	catch (FileNotFoundException e) 
-	 {
-		// TODO Auto-generated catch block
-		 System.out.println("False");
-		 
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 System.out.println();
-		 
-		 return false;
-		 
-		  
-	}
-	
-	System.out.println("False");
-	return false;
-	
-	
-}
+
+        catch (FileNotFoundException e) 
+         {
+                // TODO Auto-generated catch block
+
+                 return false;
+                 
+                  
+        }
+
+
+        return false;
 
 
 }
 
+//method sets a author filter
+public static boolean test6()
+{
+
+        String author = "Douglas Adams";
+
+  BookLoader obj = new BookLoader();
+
+  BookMapperBackend bmb = new BookMapperBackend();
+  
+    bmb.setAuthorFilter(author);
+
+        List<IBook> books;
+
+        try
+        {
+                books = obj.loadBooks("books.csv");
+
+                for(IBook b: books)
+                {
+                	bmb.addBook(b);
+                }
+                
+                for(IBook b: bmb.searchByTitleWord(""))
+                {
+                	if(!b.getAuthors().equals(author))
+                	{
+                	   return false;
+               	}
+                }
+}
+
+        catch (FileNotFoundException e)
+         {
+                // TODO Auto-generated catch block
+
+                 return false;
+        }
+
+        return true;
 
 
+}
+
+//method validates entered for isbn
+public static boolean test7()
+{
+        String isbn = "9780743264464";
+
+        BookLoader obj = new BookLoader();
+        
+        ISBNValidator val = new ISBNValidator();
+
+
+        try
+        {
+        	 List<IBook> books = obj.loadBooks("books.csv");
+
+
+
+                for(IBook b: books)
+                {
+                        if(val.validate(isbn))
+                        {
+                                return true;
+                        }
+
+                }
+
+
+}
+
+        catch (FileNotFoundException e)
+         {
+                // TODO Auto-generated catch block
+                 return false;
+
+
+        }
+
+         return false;
+}
+
+
+}
